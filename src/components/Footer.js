f309b280d1e8c83c6f0491389fa50576b182d5e2
Steven Griffith <stevengriffith@blackbetty.lan>
2024-03-02 14:52:12 -0500

    add tooltip so button has functionality

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,14 @@
-import React from "react";
+import React, {useState} from "react";
+import "../Tooltip.scss";
 
-export const  Footer = ({openHour = 12, closeHour = 22}) => {
+export const Footer = ({openHour = 12, closeHour = 22}) => {
     const hour = new Date().getHours();
     const isOpen = hour >= openHour && hour <= closeHour;
 
     return (
         <footer className="footer">
             {isOpen ? (
-                <Order closeHour={closeHour} openHour={openHour} />
+                <Order closeHour={closeHour} openHour={openHour}/>
             ) : (
                 <p>
                     We're happy to welcome you between {openHour}:00 and {closeHour}:00.
@@ -17,12 +18,35 @@ export const  Footer = ({openHour = 12, closeHour = 22}) => {
     );
 }
 
-const Order = ({ closeHour, openHour }) => (
-    <div className="order">
-        <p>
-            We're open from {openHour}:00 to {closeHour}:00. Come visit us or order
-            online.
-        </p>
-        <button className="order__action">Order</button>
-    </div>
-)
+const Tooltip = ({children, message}) => {
+    return (
+        <div className="tooltip-container">
+            {children}
+            <div className="tooltip__content">
+                {message}
+                <span className="tooltip__arrow"></span>
+            </div>
+        </div>
+    );
+};
+
+const Order = ({closeHour, openHour}) => {
+    const [isTooltipVisible, setTooltipVisible] = useState(false);
+
+    const toggleTooltip = () => {
+        setTooltipVisible(!isTooltipVisible);
+    };
+    return (
+
+        <div className="order">
+            <p>
+                We're open from {openHour}:00 to {closeHour}:00. Come visit us or order
+                online.
+            </p>
+            <button onClick={toggleTooltip} className="order__action">Order</button>
+            {isTooltipVisible && (
+                <Tooltip message="This is just a mock react site. We don't actually take orders."/>
+            )}
+        </div>
+    )
+}
